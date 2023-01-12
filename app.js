f7ffc8baf8bf9df5b7ba6efa754dad6f95bc8927
AleksandrMarkitan <107389341+AleksandrMarkitan@users.noreply.github.com>
2023-01-12 03:41:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts");
-const authRouter = require("./routes/api/auth");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
@@ -14,8 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api/contacts", contactsRouter);
-app.use("/api/auth", authRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
