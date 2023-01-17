23525bb66ec135cd496adad96d0b7fefe25a2b82
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-01-18 00:03:07 +0200

    Update cloudUploadFunc.js

--- helpers/cloudUploadFunc.js
+++ helpers/cloudUploadFunc.js
@@ -6,27 +6,24 @@ const Jimp = require("jimp");
 
 const dafaultImgURL =
   "http://res.cloudinary.com/digml0rat/image/upload/v1673906206/Fullstack%20Group%20Project/home-pets_hywfgq.png";
-  
-  const options = {
-    use_filename: true,
-    unique_filename: true,
-    overwrite: true,
-  };
+
+const options = {
+  use_filename: true,
+  unique_filename: true,
+  overwrite: true,
+  transformation: [{ width: 336, height: 336, gravity: "faces", crop: "fill" }],
+};
 
 // завантаження зображення у хмару
 const uploadImg = async (tempUpload) => {
-  if(tempUpload){
-    const resizeAvatar = await Jimp.read(tempUpload);
-    resizeAvatar.resize(250, 250).write(tempUpload); 
-    // Зробити оптимізацію зображення у клоуденарі якщо буде час
-  
+  if (tempUpload) {
     const upload = await cloudinary.uploader.upload(tempUpload, options);
-  
+
     // видаляємо з папки temp
     await fs.unlink(tempUpload);
-    
+
     return upload.secure_url;
-  } 
+  }
   return dafaultImgURL;
 };
 
