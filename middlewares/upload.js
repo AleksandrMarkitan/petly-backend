25573d9a2de25c260b2cdaa06511839660336840
anastasiia-matushenko <muzaam07@gmail.com>
2023-01-18 12:51:05 +0200

    fix

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,15 +1,16 @@
 const multer = require("multer");
 const path = require("path");
 
+const { CLIENT_MAX_BODY_SIZE } = process.env;
+
 const tempDir = path.join(__dirname, "../", "temp");
-const client_max_body_size = 500000;
 const multerConfig = multer.diskStorage({
   destination: tempDir,
 });
 
 const upload = multer({
   storage: multerConfig,
-  limits: { fileSize: client_max_body_size },
+  limits: { fileSize: CLIENT_MAX_BODY_SIZE },
 });
 
 module.exports = upload;
