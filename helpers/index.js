357f5f4f10183f4fbce9bfe2c9f260e3a4fc2fd6
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-18 16:42:05 +0200

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -3,6 +3,7 @@ const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
 const cloudinary = require("./cloudinary");
 const uploadImg = require("./cloudUploadFunc");
+const uploadImgPet = require("./cloudUploadPet");
 
 module.exports = {
   HttpError,
@@ -10,4 +11,5 @@ module.exports = {
   handleMongooseError,
   cloudinary,
   uploadImg,
+  uploadImgPet,
 };
